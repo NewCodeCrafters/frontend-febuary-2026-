a404e7a591c332c138622b4f6e32842b8d896c42
gideon-del <gideonchidi471@gmail.com>
2026-05-13 12:05:53 +0100

    feat: done with variables

---
+++ JS/variables/data-types.js
@@ -0,0 +1,14 @@
+"use strict";
+// Number
+let n = 20; // Integer
+console.log(n + 10);
+console.log(n * 10);
+console.log(n / 10);
+console.log(n - 10);
+n = 10.4; // Float
+n = 3;
+// String
+let str = "Hi";
+str = "Hello";
+str = `The result of ${n} * 3 is ${n * 3}`;
+console.log(str);
---
+++ JS/variables/variables.js
@@ -0,0 +1,16 @@
+"use strict";
+// Declaration
+// let message;
+// // console.log(message);
+// message = "Hello Jonathan";
+// console.log(message);
+let message = "Hello Jonathan and Praise";
+// console.log(message);
+message = "Hello Gideon";
+console.log(message);
+let hello = message;
+console.log(hello);
+// let message;
+
+// jonathan_praise snake_case
+// jonathanPraise camelCase
