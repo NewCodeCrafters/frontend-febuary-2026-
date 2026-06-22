7b77b8036b589ac8229594e6f83935f7d2262c27
gideon-del <gideonchidi471@gmail.com>
2026-06-22 12:29:40 +0100

    feat: done with event

--- projects/news-homepage-main/js/index.js
+++ projects/news-homepage-main/js/index.js
@@ -10,3 +10,21 @@ const newsContainerEl = document.querySelector(".news");
 const articles = newsContainerEl.querySelectorAll(".new-article");
 // articles[0].firstElementChild
 // const title = document.getElementById("hydrogen");
+
+const overlayEl = document.querySelector(".overlay");
+const navContainerEl = document.querySelector(".nav-container");
+const closeBtn = document.querySelector(".close-btn");
+const openBtn = document.querySelector(".open-btn");
+
+openBtn.addEventListener("click", () => {
+  //   console.log(overlayEl.classList);
+  //   console.log(navContainerEl.classList);
+
+  overlayEl.classList.remove("hide");
+  navContainerEl.classList.remove("hide");
+});
+
+closeBtn.addEventListener("click", () => {
+  overlayEl.classList.add("hide");
+  navContainerEl.classList.add("hide");
+});
